f124e2e4316c5150601bc975d2e8b35a3c580f8b
Ryan Teng <ryanrteng@gmail.com>
2020-03-26 03:06:45 -0400

    Fixing heroku build error

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,19 +3,19 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = async () => {
-    const response = await fetch('http://wbdv-generic-server.herokuapp.com/api/001642349/courses');
+    const response = await fetch('https://wbdv-generic-server.herokuapp.com/api/001642349/courses');
     return await response.json();
   };
   findCourseById = async (cid) => {
-    const response = await fetch('http://wbdv-generic-server.herokuapp.com/api/001642349/courses/' + cid);
+    const response = await fetch('https://wbdv-generic-server.herokuapp.com/api/001642349/courses/' + cid);
     return await response.json();
   };
   findModulesForCourse = async(cid) => {
-    const response = await fetch('http://wbdv-generic-server.herokuapp.com/api/001642349/courses/' + cid + '/modules');
+    const response = await fetch('https://wbdv-generic-server.herokuapp.com/api/001642349/courses/' + cid + '/modules');
     return await response.json();
   }
   findLessonsForModule = async(mid) => {
-    const response = await fetch('http://wbdv-generic-server.herokuapp.com/api/001642349/modules/' + mid + '/lessons');
+    const response = await fetch('https://wbdv-generic-server.herokuapp.com/api/001642349/modules/' + mid + '/lessons');
     return await response.json();
   }
 }
